096b1c6f7588923fbe116f136f7f6994f7e04ead
Sandeep <sandeepsandy958168@gmail.com>
2025-06-03 17:31:35 +0530

    Commit bash script

---
+++ greet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+echo "Enter your firstname: "
+read fn
+echo "Enter your lasst name: "
+read ln
+echo "Hello $fn $ln , Welcome to our company..!!
+Congrats on your new role as DEVOPS ENGINEER"
