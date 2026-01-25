65b1d0b5ca706a8a20046cb138f200892b3312ec
Uday143Kumar <u3kumar@gmail.com>
2026-01-25 13:38:44 +0530

    Update HelloServlet.java

--- mywebapp/src/main/java/com/example/HelloServlet.java
+++ mywebapp/src/main/java/com/example/HelloServlet.java
@@ -5,9 +5,11 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
+        response.setContentType("text/plain");
         response.getWriter().println("Hello, World!");
     }
 }
